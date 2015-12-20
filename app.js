778fe66fdc4f7868609e508a4fcb288d6c3233a1
Tuan Pham <tuan.pham@hellopay.com>
2015-12-20 09:49:13 +0700

    add sitemap.xml

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var fs = require('fs');
-var FileStreamRotator = require('file-stream-rotator')
+var FileStreamRotator = require('file-stream-rotator');
 
 var logDirectory = __dirname + '/log';
 
