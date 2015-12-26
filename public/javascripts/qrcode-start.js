6956b239d26cb487a41c69fde9129c0850587fc8
Tuan Pham <tuan.pham@hellopay.com>
2015-12-27 00:50:58 +0700

    edit description

--- public/javascripts/qrcode-start.js
+++ public/javascripts/qrcode-start.js
@@ -5,6 +5,7 @@ $(function () {
     qrcode.makeCode('example');
 
     window.setTimeout(function(){
+        $('#qr_code_output>img').attr('alt', 'QR Code generator result');
         var data = $('#qr_code_output>img').attr('src');
         console.log(data);
         $('#qr_download_btn').attr('href', data);
